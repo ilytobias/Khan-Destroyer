a1c2dc0916fbd72beeb7edbcdd28452c8846577f
Tobias Wilson <165577429+ilytobias@users.noreply.github.com>
2024-10-06 09:34:00 -0700

    Update overwrite.user.js

--- cheats/overwrite.user.js
+++ cheats/overwrite.user.js
@@ -7,15 +7,39 @@
 // @grant        none
 // ==/UserScript==
 
-let e = JSON.parse;
+if(typeof originalParse ==="undefined") window.originalParse = JSON.parse;
+console.log("if you look in the console you WILL see a error, they save it as a question though so I'm not worried about it right now.")
 JSON.parse = function (a, t) {
-  let n = e(a, t);
+  let n = originalParse(a, t);
   try {
-    if (n && n.data && n.data.assessmentItem && n.data.assessmentItem.item && n.data.assessmentItem.item.itemData) {
-      n.data.assessmentItem.item.itemData = "{\"answerArea\":{\"calculator\":false,\"chi2Table\":false,\"periodicTable\":false,\"tTable\":false,\"zTable\":false},\"hints\":[{\"content\":\"$\\\\\\\\begin{align}\\\\n\\\\\\\\left(\\\\\\\\dfrac{z^{4}}{6^{2}}\\\\\\\\right)^{-3}&=\\\\\\\\dfrac{\\\\\\\\left(z^{4}\\\\\\\\right)^{-3}}{\\\\\\\\left(6^{2}\\\\\\\\right)^{-3}}\\\\n\\\\\\\\end{align}$\",\"images\":{},\"replace\":false,\"widgets\":{}},{\"content\":\"$\\\\\\\\begin{align}\\\\n\\\\\\\\phantom{\\\\\\\\left(\\\\\\\\dfrac{z^{4}}{6^{2}}\\\\\\\\right)^{-3}}&=\\\\\\\\dfrac{z^{(4)(-3)}}{6^{(2)(-3)}}\\\\n\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\n&=\\\\\\\\dfrac{z^{-12}}{6^{-6}}\\\\n\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\n&=\\\\\\\\dfrac{6^{6}}{z^{12}}\\\\n\\\\\\\\end{align}$\",\"images\":{},\"replace\":false,\"widgets\":{}}],\"itemDataVersion\":{\"major\":0,\"minor\":1},\"question\":{\"content\":\"free young thug made by ilyTobias[[☃ radio 1]]\",\"images\":{},\"widgets\":{\"radio 1\":{\"alignment\":\"default\",\"graded\":true,\"options\":{\"choices\":[{\"content\":\"Correct answer\",\"correct\":true},{\"content\":\"Incorrect answer\",\"correct\":false}],\"deselectEnabled\":false,\"displayCount\":null,\"hasNoneOfTheAbove\":false,\"multipleSelect\":false,\"onePerLine\":true,\"randomize\":false},\"static\":false,\"type\":\"radio\",\"version\":{\"major\":1,\"minor\":0}}}}}";
+    // Parse the nested data structure
+    const lessonData = JSON.parse(n.data.assessmentItem.item.itemData);
+        if (lessonData.question && lessonData.question.content && 
+        lessonData.question.content[1] === lessonData.question.content[1].toUpperCase()) {
+      console.log(lessonData);
+      lessonData.question.content=location.hostname.split(".")[0]==="pt"?"Selecione uma opção de resposta.":"Please select a answer choice."+"\n [[☃ radio 1]] [[☃ explanation 1]]"
+        lessonData.question.widgets={
+          "radio 1": {
+            "options": {
+              "choices": [
+                { "content": location.hostname.split(".")[0]==="pt"?"Correcto":"Correct", "correct": true },
+                { "content": location.hostname.split(".")[0]==="pt"?"Incorrecto":"Incorrect", "correct": false }
+              ]
+            }
+          },
+          "explanation 1": {
+            "options": {
+              "explanation": "discord.gg/khanacademy",
+                hidePrompt: "", 
+                showPrompt: "Discord"
+            }
+          }
+        };
+      n.data.assessmentItem.item.itemData = JSON.stringify(lessonData);
     }
-  } catch (r) {
-    console.error("Error modifying parsed data:", r);
-  }
+    
+  } catch (r) {}
+  
   return n;
 };
+console.error=function(){} //gets rid of the retarded error from khan
