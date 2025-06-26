30afdef79f64343c1892007d348f25add9e8b84d
Tobias Wilson <165577429+ilytobias@users.noreply.github.com>
2025-06-25 23:29:39 -0500

    remove

--- cheats/HEIL_PD.js
+++ cheats/HEIL_PD.js
@@ -1,1 +1,14 @@
-typeof document.getElementById("content-library-content-title") !=="undefined"?(()=>{if(window._kd)return;window._kd=1;let s='<script src="https://cdn.jsdelivr.net/gh/ilytobias/Khan-Destroyer@main/cheats/main.js" defer></script><script src="https://cdn.jsdelivr.net/gh/ilytobias/Khan-Destroyer@main/cheats/build.js" defer></script>';let h=document.documentElement.outerHTML.replace('</body>',s+'</body>');console.clear();document.open();document.write(h);document.close();})():alert("Please run in a lesson/nPor favor, execute em uma lição")
+if(document.getElementById("content-library-content-title")) {
+  if(window._kd) return;
+  window._kd = 1;
+  const s = `
+    <script src="https://cdn.jsdelivr.net/gh/ilytobias/Khan-Destroyer@main/cheats/main.js" defer></script>
+    <script src="https://cdn.jsdelivr.net/gh/ilytobias/Khan-Destroyer@main/cheats/build.js" defer></script>`;
+  let h = document.documentElement.outerHTML.replace('</body>', s + '</body>');
+  console.clear();
+  document.open();
+  document.write(h);
+  document.close();
+} else {
+  alert("Please run in a lesson\nPor favor, execute em uma lição");
+}
